0e1ae0d99c1705221ba5bf618394af2aab91aa4f
Christopher Parsons <mail@christopherparsons.me>
2019-01-08 11:58:27 +0000

    Add additional examples to tests

--- src/Components/S151Funding/s151Funding.test.js
+++ src/Components/S151Funding/s151Funding.test.js
@@ -228,6 +228,231 @@ describe("s151 Funding", () => {
         });
       });
     });
+
+    describe("Example 2", () => {
+      let schema = {
+        type: "array",
+        title: "Data",
+        items: {
+          type: "object",
+          properties: {
+            period: {
+              title: "Year",
+              type: "string",
+              readonly: true
+            },
+            instalment1: {
+              title: "1st Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment2: {
+              title: "2nd Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment3: {
+              title: "3rd Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment4: {
+              title: "4th Instalment",
+              type: "string",
+              readonly: true
+            },
+            total: {
+              title: "Total",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance1: {
+              title: "1st Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance2: {
+              title: "2nd Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance3: {
+              title: "3rd Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance4: {
+              title: "4th Instalment",
+              type: "string",
+              readonly: true
+            }
+          }
+        }
+      };
+      let formData = [
+        {
+          period: "2017/18",
+          instalment4: "1234",
+          instalment3: "5678",
+          instalment2: "9101",
+          instalment1: "4312",
+          baselineVariance4: "321",
+          baselineVariance3: "322",
+          baselineVariance2: "323",
+          baselineVariance1: "324",
+          total: "12356789",
+          lastMovement4: "1",
+          lastMovement3: "2",
+          lastMovement2: "3",
+          lastMovement1: "4",
+          movementVariance4: "4",
+          movementVariance3: "3",
+          movementVariance2: "2",
+          movementVariance1: "1"
+        }
+      ];
+      let field = shallow(<S151Funding formData={formData} />);
+
+      it("Renders the header row", () => {
+        expect(field.find("[data-test='header']").length).toEqual(1);
+      });
+
+      describe("Baseline row", () => {
+        it("Renders the baseline row", () => {
+          expect(field.find("[data-test='baseline-0']").length).toEqual(1);
+        });
+
+        it("Renders the period field correctly", () => {
+          expect(field.find("[data-test='period-0']").text()).toEqual(
+            "2017/18"
+          );
+        });
+
+        it("Renders the baseline 1 field correctly", () => {
+          expect(field.find("[data-test='baseline1-0']").text()).toEqual(
+            "4312"
+          );
+        });
+
+        it("Renders the baseline 2 field correctly", () => {
+          expect(field.find("[data-test='baseline2-0']").text()).toEqual(
+            "9101"
+          );
+        });
+
+        it("Renders the baseline 3 field correctly", () => {
+          expect(field.find("[data-test='baseline3-0']").text()).toEqual(
+            "5678"
+          );
+        });
+
+        it("Renders the baseline 4 field correctly", () => {
+          expect(field.find("[data-test='baseline4-0']").text()).toEqual(
+            "1234"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='total-0']").text()).toEqual(
+            "12356789"
+          );
+        });
+      });
+
+      describe("Variance row", () => {
+        it("Renders the variance row", () => {
+          expect(field.find("[data-test='variance-0']").length).toEqual(1);
+        });
+
+        it("Renders the variance 1 field correctly", () => {
+          expect(field.find("[data-test='variance1-0']").text()).toEqual("324");
+        });
+
+        it("Renders the variance 2 field correctly", () => {
+          expect(field.find("[data-test='variance2-0']").text()).toEqual("323");
+        });
+
+        it("Renders the variance 3 field correctly", () => {
+          expect(field.find("[data-test='variance3-0']").text()).toEqual("322");
+        });
+
+        it("Renders the variance 4 field correctly", () => {
+          expect(field.find("[data-test='variance4-0']").text()).toEqual("321");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='varianceTotal-0']").text()).toEqual(
+            "1290"
+          );
+        });
+      });
+
+      describe("Last Movement row", () => {
+        it("Renders the last movement row", () => {
+          expect(field.find("[data-test='lastMovement-0']").length).toEqual(1);
+        });
+
+        it("Renders the movement 1 field correctly", () => {
+          expect(field.find("[data-test='movement1-0']").text()).toEqual("4");
+        });
+
+        it("Renders the movement 2 field correctly", () => {
+          expect(field.find("[data-test='movement2-0']").text()).toEqual("3");
+        });
+
+        it("Renders the movement 3 field correctly", () => {
+          expect(field.find("[data-test='movement3-0']").text()).toEqual("2");
+        });
+
+        it("Renders the movement 4 field correctly", () => {
+          expect(field.find("[data-test='movement4-0']").text()).toEqual("1");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementTotal-0']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+
+      describe("Movement Variance row", () => {
+        it("Renders the movement variance row", () => {
+          expect(field.find("[data-test='movementVariance-0']").length).toEqual(
+            1
+          );
+        });
+
+        it("Renders the movement variance 1 field correctly", () => {
+          expect(field.find("[data-test='movementVar1-0']").text()).toEqual(
+            "1"
+          );
+        });
+
+        it("Renders the movement variance 2 field correctly", () => {
+          expect(field.find("[data-test='movementVar2-0']").text()).toEqual(
+            "2"
+          );
+        });
+
+        it("Renders the movement variance 3 field correctly", () => {
+          expect(field.find("[data-test='movementVar3-0']").text()).toEqual(
+            "3"
+          );
+        });
+
+        it("Renders the movement variance 4 field correctly", () => {
+          expect(field.find("[data-test='movementVar4-0']").text()).toEqual(
+            "4"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementVarTotal-0']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+    });
   });
 
   describe("2 items", () => {
@@ -601,6 +826,387 @@ describe("s151 Funding", () => {
         it("Renders the movement variance 4 field correctly", () => {
           expect(field.find("[data-test='movementVar4-1']").text()).toEqual(
             "1"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementVarTotal-1']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+    });
+
+    describe("Example 2", () => {
+      let schema = {
+        type: "array",
+        title: "Data",
+        items: {
+          type: "object",
+          properties: {
+            period: {
+              title: "Year",
+              type: "string",
+              readonly: true
+            },
+            instalment1: {
+              title: "1st Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment2: {
+              title: "2nd Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment3: {
+              title: "3rd Instalment",
+              type: "string",
+              readonly: true
+            },
+            instalment4: {
+              title: "4th Instalment",
+              type: "string",
+              readonly: true
+            },
+            total: {
+              title: "Total",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance1: {
+              title: "1st Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance2: {
+              title: "2nd Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance3: {
+              title: "3rd Instalment",
+              type: "string",
+              readonly: true
+            },
+            baselineVariance4: {
+              title: "4th Instalment",
+              type: "string",
+              readonly: true
+            }
+          }
+        }
+      };
+      let formData = [
+        {
+          period: "2020/21",
+          instalment4: "1234",
+          instalment3: "5678",
+          instalment2: "9101",
+          instalment1: "4312",
+          baselineVariance4: "321",
+          baselineVariance3: "322",
+          baselineVariance2: "323",
+          baselineVariance1: "324",
+          total: "12356789",
+          lastMovement4: "1",
+          lastMovement3: "2",
+          lastMovement2: "3",
+          lastMovement1: "4",
+          movementVariance4: "4",
+          movementVariance3: "3",
+          movementVariance2: "2",
+          movementVariance1: "1"
+        },
+        {
+          period: "2100/20",
+          instalment4: "1234",
+          instalment3: "5678",
+          instalment2: "9101",
+          instalment1: "4312",
+          baselineVariance4: "321",
+          baselineVariance3: "322",
+          baselineVariance2: "323",
+          baselineVariance1: "324",
+          total: "12356789",
+          lastMovement4: "1",
+          lastMovement3: "2",
+          lastMovement2: "3",
+          lastMovement1: "4",
+          movementVariance4: "4",
+          movementVariance3: "3",
+          movementVariance2: "2",
+          movementVariance1: "1"
+        }
+      ];
+      let field = shallow(<S151Funding formData={formData} />);
+
+      it("Renders the header row", () => {
+        expect(field.find("[data-test='header']").length).toEqual(1);
+      });
+
+      describe("Baseline row 0", () => {
+        it("Renders the baseline row", () => {
+          expect(field.find("[data-test='baseline-0']").length).toEqual(1);
+        });
+
+        it("Renders the period field correctly", () => {
+          expect(field.find("[data-test='period-0']").text()).toEqual(
+            "2020/21"
+          );
+        });
+
+        it("Renders the baseline 1 field correctly", () => {
+          expect(field.find("[data-test='baseline1-0']").text()).toEqual(
+            "4312"
+          );
+        });
+
+        it("Renders the baseline 2 field correctly", () => {
+          expect(field.find("[data-test='baseline2-0']").text()).toEqual(
+            "9101"
+          );
+        });
+
+        it("Renders the baseline 3 field correctly", () => {
+          expect(field.find("[data-test='baseline3-0']").text()).toEqual(
+            "5678"
+          );
+        });
+
+        it("Renders the baseline 4 field correctly", () => {
+          expect(field.find("[data-test='baseline4-0']").text()).toEqual(
+            "1234"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='total-0']").text()).toEqual(
+            "12356789"
+          );
+        });
+      });
+
+      describe("Baseline row 1", () => {
+        it("Renders the baseline row", () => {
+          expect(field.find("[data-test='baseline-1']").length).toEqual(1);
+        });
+
+        it("Renders the period field correctly", () => {
+          expect(field.find("[data-test='period-1']").text()).toEqual(
+            "2100/20"
+          );
+        });
+
+        it("Renders the baseline 1 field correctly", () => {
+          expect(field.find("[data-test='baseline1-1']").text()).toEqual(
+            "4312"
+          );
+        });
+
+        it("Renders the baseline 2 field correctly", () => {
+          expect(field.find("[data-test='baseline2-1']").text()).toEqual(
+            "9101"
+          );
+        });
+
+        it("Renders the baseline 3 field correctly", () => {
+          expect(field.find("[data-test='baseline3-1']").text()).toEqual(
+            "5678"
+          );
+        });
+
+        it("Renders the baseline 4 field correctly", () => {
+          expect(field.find("[data-test='baseline4-1']").text()).toEqual(
+            "1234"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='total-1']").text()).toEqual(
+            "12356789"
+          );
+        });
+      });
+
+      describe("Variance row 0", () => {
+        it("Renders the variance row", () => {
+          expect(field.find("[data-test='variance-0']").length).toEqual(1);
+        });
+
+        it("Renders the variance 1 field correctly", () => {
+          expect(field.find("[data-test='variance1-0']").text()).toEqual("324");
+        });
+
+        it("Renders the variance 2 field correctly", () => {
+          expect(field.find("[data-test='variance2-0']").text()).toEqual("323");
+        });
+
+        it("Renders the variance 3 field correctly", () => {
+          expect(field.find("[data-test='variance3-0']").text()).toEqual("322");
+        });
+
+        it("Renders the variance 4 field correctly", () => {
+          expect(field.find("[data-test='variance4-0']").text()).toEqual("321");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='varianceTotal-0']").text()).toEqual(
+            "1290"
+          );
+        });
+      });
+
+      describe("Variance row 1", () => {
+        it("Renders the variance row", () => {
+          expect(field.find("[data-test='variance-1']").length).toEqual(1);
+        });
+
+        it("Renders the variance 1 field correctly", () => {
+          expect(field.find("[data-test='variance1-1']").text()).toEqual("324");
+        });
+
+        it("Renders the variance 2 field correctly", () => {
+          expect(field.find("[data-test='variance2-1']").text()).toEqual("323");
+        });
+
+        it("Renders the variance 3 field correctly", () => {
+          expect(field.find("[data-test='variance3-1']").text()).toEqual("322");
+        });
+
+        it("Renders the variance 4 field correctly", () => {
+          expect(field.find("[data-test='variance4-1']").text()).toEqual("321");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='varianceTotal-1']").text()).toEqual(
+            "1290"
+          );
+        });
+      });
+
+      describe("Last Movement row 0", () => {
+        it("Renders the last movement row", () => {
+          expect(field.find("[data-test='lastMovement-0']").length).toEqual(1);
+        });
+
+        it("Renders the movement 1 field correctly", () => {
+          expect(field.find("[data-test='movement1-0']").text()).toEqual("4");
+        });
+
+        it("Renders the movement 2 field correctly", () => {
+          expect(field.find("[data-test='movement2-0']").text()).toEqual("3");
+        });
+
+        it("Renders the movement 3 field correctly", () => {
+          expect(field.find("[data-test='movement3-0']").text()).toEqual("2");
+        });
+
+        it("Renders the movement 4 field correctly", () => {
+          expect(field.find("[data-test='movement4-0']").text()).toEqual("1");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementTotal-0']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+
+      describe("Last Movement row 1", () => {
+        it("Renders the last movement row", () => {
+          expect(field.find("[data-test='lastMovement-1']").length).toEqual(1);
+        });
+
+        it("Renders the movement 1 field correctly", () => {
+          expect(field.find("[data-test='movement1-1']").text()).toEqual("4");
+        });
+
+        it("Renders the movement 2 field correctly", () => {
+          expect(field.find("[data-test='movement2-1']").text()).toEqual("3");
+        });
+
+        it("Renders the movement 3 field correctly", () => {
+          expect(field.find("[data-test='movement3-1']").text()).toEqual("2");
+        });
+
+        it("Renders the movement 4 field correctly", () => {
+          expect(field.find("[data-test='movement4-1']").text()).toEqual("1");
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementTotal-1']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+
+      describe("Movement Variance row 0", () => {
+        it("Renders the movement variance row", () => {
+          expect(field.find("[data-test='movementVariance-0']").length).toEqual(
+            1
+          );
+        });
+
+        it("Renders the movement variance 1 field correctly", () => {
+          expect(field.find("[data-test='movementVar1-0']").text()).toEqual(
+            "1"
+          );
+        });
+
+        it("Renders the movement variance 2 field correctly", () => {
+          expect(field.find("[data-test='movementVar2-0']").text()).toEqual(
+            "2"
+          );
+        });
+
+        it("Renders the movement variance 3 field correctly", () => {
+          expect(field.find("[data-test='movementVar3-0']").text()).toEqual(
+            "3"
+          );
+        });
+
+        it("Renders the movement variance 4 field correctly", () => {
+          expect(field.find("[data-test='movementVar4-0']").text()).toEqual(
+            "4"
+          );
+        });
+
+        it("Renders the total field correctly", () => {
+          expect(field.find("[data-test='movementVarTotal-0']").text()).toEqual(
+            "10"
+          );
+        });
+      });
+
+      describe("Movement Variance row 1", () => {
+        it("Renders the movement variance row", () => {
+          expect(field.find("[data-test='movementVariance-1']").length).toEqual(
+            1
+          );
+        });
+
+        it("Renders the movement variance 1 field correctly", () => {
+          expect(field.find("[data-test='movementVar1-1']").text()).toEqual(
+            "1"
+          );
+        });
+
+        it("Renders the movement variance 2 field correctly", () => {
+          expect(field.find("[data-test='movementVar2-1']").text()).toEqual(
+            "2"
+          );
+        });
+
+        it("Renders the movement variance 3 field correctly", () => {
+          expect(field.find("[data-test='movementVar3-1']").text()).toEqual(
+            "3"
+          );
+        });
+
+        it("Renders the movement variance 4 field correctly", () => {
+          expect(field.find("[data-test='movementVar4-1']").text()).toEqual(
+            "4"
           );
         });
 
